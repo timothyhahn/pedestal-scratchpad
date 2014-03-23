8c2dda065a06fcdb023b6c3d3c11bc87ffb153f5
Timothy Hahn <timyhahn@gmail.com>
2014-03-22 21:49:38 -0400

    Fixed lil bug

--- app/src/tutorial_client/behavior.clj
+++ app/src/tutorial_client/behavior.clj
@@ -1,21 +1,21 @@
 (ns ^:shared tutorial-client.behavior
-    (:require [clojure.string :as string]
-              [io.pedestal.app.messages :as msg]
-              [io.pedestal.app :as app]))
+      (:require [clojure.string :as string]
+                              [io.pedestal.app.messages :as msg]
+                              [io.pedestal.app :as app]))
 
 (defn inc-transform [old-value _]
-  ((fnil inc 0) old-value))
+    ((fnil inc 0) old-value))
+
+(defn swap-transform [_ message]
+    (:value message))
 
 (defn init-main [_]
-  [[:transform-enable [:main :my-counter] :inc [{msg/topic [:my-counter]}]]])
-
-(def swap-transform [_ message]
-  (:value message))
+    [[:transform-enable [:main :my-counter] :inc [{msg/topic [:my-counter]}]]])
 
 (def example-app
-  {:version 2
-   :transform [[:inc  [:my-counter] inc-transform]
-               [:swap [:**]         swap-transform]]
-   :emit [{:init init-main}
-          [#{[:my-counter] [:other-counters :*]} (app/default-emitter [:main])]]})
+    {:version 2
+        :transform [[:inc  [:my-counter] inc-transform]
+                                   [:swap [:**]         swap-transform]]
+        :emit [{:init init-main}
+                         [#{[:my-counter] [:other-counters :*]} (app/default-emitter [:main])]]})
 
